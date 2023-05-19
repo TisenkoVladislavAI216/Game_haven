cb6cd75bcc840033959f41e02167f4e2a448bb82
TisenkoVladislavAI216 <113099644+TisenkoVladislavAI216@users.noreply.github.com>
2023-05-20 00:04:56 +0300

    Add files via upload

--- registration.php
+++ registration.php
@@ -8,7 +8,7 @@
   $name = filter_var(trim($_POST['name']),
   FILTER_SANITIZE_STRING);
 
-  if(mb_strlen($login) < 6 || mb_strlen($login) > 20){
+  if(mb_strlen($login) < 6 || mb_strlen($login) > 32){
     echo "Помилка! Логін має бути від 6 до 20 символів";
     exit;
   }
@@ -18,7 +18,7 @@
   }
 
   if(mb_strlen($name) < 3 || mb_strlen($name) > 20){
-    echo "Помилка! Пароль має бути від 3 до 20 символів";
+    echo "Помилка! Ім'я має бути від 3 до 20 символів";
     exit;
   }
 
@@ -28,6 +28,17 @@
   $mysql->query("INSERT INTO `users` (`login`, `pass`, `name`)
   VALUES('$login', '$pass', '$name')");
 
-  $mysql->close();
+  $result = $mysql->query("SELECT * FROM `users`
+  WHERE `login` = '$login' AND `pass` = '$pass'");
+
+  $user = $result->fetch_assoc();
+
+  if(count($user) == 0) {
+    echo "Такого користувача не знайдено!";
+    exit;
+  }
+
+  setcookie('user', $user['name'], time() + 60*60, "/");
+
   header('Location: /main.php');
 ?>
